916643bc3502eab5fcb5c2cf958851969c3c1aff
Rosa Piscitelli <piscitelli.rosa98@gmail.com>
2024-09-27 15:42:45 +0200

    syso hello world

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -3,7 +3,7 @@ package it.generationitaly.app.controller;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
 
 	}
 
