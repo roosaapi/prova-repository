d3f7248622f957f382350001faa29c1fbafb0f78
Rosa Piscitelli <piscitelli.rosa98@gmail.com>
2024-09-27 15:52:11 +0200

    add metodo2

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -5,9 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		metodo1();
+		metodo2();
 	}
 
 	private static void metodo1() {
 		System.out.println("ciao");
+	}
+	
+	private static void metodo2() {
+		System.out.println("ciao2");
 	}
 }
